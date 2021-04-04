0571b99b08e5add29dc8b443036d7afeacaf0f79
talasago <lyricaldragons@gmail.com>
2021-04-04 17:25:56 +0900

    GET,POST,PUT,DELETEno

---
+++ put.py
@@ -0,0 +1,15 @@
+import json
+
+
+def hello(event, context):
+    body = {
+        "message": "Go Serverless v1.0! Your function executed PUT method successfully!",
+        "input": event
+    }
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
+
+    return response
